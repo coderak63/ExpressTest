37e23bb2823484fad02442db36a43673d441c7ac
Abhishek Singh <abhishek.mnnit.mca2k22@gmail.com>
2024-02-05 16:28:21 +0530

    added Dockerfile, package.json, gitignore

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello - from Express JS!')
+})
+
+app.get('/abhi', (req, res) => {
+    res.send('Hi, Abhishek here!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
